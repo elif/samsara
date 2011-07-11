490ee6cf2b366b3fe17ac2eecece07886a8d86bb
Eli Foley <eli@vitrue.com>
2011-07-10 22:30:22 -0400

    Version 0.0.1 - working splitter, primative logging

--- samsara.js
+++ samsara.js
@@ -1,21 +1,57 @@
-var http = require('http');
+var http = require('http'),
+    url = require('url');
 
 http.createServer(function(request, response) {
-  var proxy = http.createClient(80, request.headers['host'])
-  var proxy_request = proxy.request(request.method, request.url, request.headers);
-  proxy_request.addListener('response', function (proxy_response) {
-    proxy_response.addListener('data', function(chunk) {
-      response.write(chunk, 'binary');
+  console.log("got request: " + request.headers['host'] + request_path(request));
+  var emcee_request = http.request({
+    host: process.env.EMCEE_HOST,
+    path: request_path(request),
+    port: parseInt(process.env.EMCEE_PORT),
+    method: request.method,
+    headers: request.headers });
+
+  var deejay_request = http.request({
+    host: process.env.DEEJAY_HOST,
+    path: request_path(request),
+    port: parseInt(process.env.DEEJAY_PORT),
+    method: request.method,
+    headers: request.headers });
+
+  emcee_request.on('response', function(emcee_response) {
+    emcee_response.on('data', function(chunk) { 
+      response.write(chunk, 'binary'); 
+      record_response("emcee", request.headers['host'] + request.url, emcee_response.statusCode, chunk.toString('binary')) });
+    emcee_response.on('end', function() { response.end(); });
+    response.writeHead(emcee_response.statusCode, emcee_response.headers);
+  });
+
+  deejay_request.on('response', function(deejay_response) {
+    deejay_response.on('data', function(chunk) { 
+      record_response("deejay", request.headers['host'] + request.url, deejay_response.statusCode, chunk.toString('utf8')) 
     });
-    proxy_response.addListener('end', function() {
-      response.end();
-    });
-    response.writeHead(proxy_response.statusCode, proxy_response.headers);
   });
-  request.addListener('data', function(chunk) {
-    proxy_request.write(chunk, 'binary');
+
+  request.on('data', function(chunk) {
+    emcee_request.write(chunk, 'binary');
+    deejay_request.write(chunk, 'binary');
   });
-  request.addListener('end', function() {
-    proxy_request.end();
+  request.on('end', function() {
+    emcee_request.end();
+    deejay_request.end();
   });
-}).listen(8080);
+  request.on('close', function() {
+    console.log("Connection terminated before expected");
+  });
+}).listen(80);
+
+function request_path(request) {
+  var parsed_url = url.parse(request.url)
+  return parsed_url['search'] ? (parsed_url['pathname'] + parsed_url['search']) : parsed_url['pathname']
+}
+
+function record_response(type, url, code, body) {
+  console.log(type + ":");
+  console.log(url);
+  console.log(code);
+  console.log(body); 
+}
