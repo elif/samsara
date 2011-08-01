645c7cef8615153f8e1686342a2e26386445559c
Eli Foley <eli@vitrue.com>
2011-08-01 15:33:59 -0400

    correctly set x-forwarded-for. Refactor proxy request code out of response logic

--- samsara.js
+++ samsara.js
@@ -11,8 +11,31 @@ process.on('uncaughtException', function (err) {
 });
 
 var server = http.createServer(function(request, response) {
-  path = request_path(request);
-  if (path == "/monitor/health") { serve(response, "Healthy!!"); }
+  path = request_path(request),
+  agent = http.getAgent(process.env.EMCEE_HOST, process.env.EMCEE_PORT);
+
+  if (path == "/monitor/health") { 
+    serve(response, "Healthy!!");
+  } else if (agent.queue.length > 100) {
+    serve_502(response, "Server overloaded at the moment, please try again later");
+  } else {
+    if (redis_client.sismember("samsara_url_whitelist", path)) {
+      proxy_to_deejay(request, response);
+    } else {
+      proxy_request(request, response);
+    }
+  }
+});
+
+fugue.start(server, process.env.SAMSARA_PORT, "0.0.0.0", process.env.SAMSARA_WORKER_COUNT, {
+  verbose: true,
+  uid: process.env.SAMSARA_UID,
+  gid: process.env.SAMSARA_GID,
+  master_pid_path: "/var/run/vitrue/samsara.pid"
+});
+ 
+function proxy_request(request, response) {
+  request.headers['x-forwarded-for'] = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
   var emcee_request = http.request({
     host: process.env.EMCEE_HOST,
     path: path,
@@ -71,16 +94,34 @@ var server = http.createServer(function(request, response) {
   deejay_request.on('error', function(error) {
     console.log("deejay request signaled error: " + error);
   });
-});
+}
 
-fugue.start(server, process.env.SAMSARA_PORT, "0.0.0.0", process.env.SAMSARA_WORKER_COUNT, {
-  verbose: true,
-  uid: process.env.SAMSARA_UID,
-  gid: process.env.SAMSARA_GID,
-//  log_file: process.env.SAMSARA_LOG,
-  master_pid_path: "/var/run/vitrue/samsara.pid"
-});
- 
+function proxy_to_deejay(request, response) {
+  request.headers['x-forwarded-for'] = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+  var deejay_request = http.request({
+    host: process.env.DEEJAY_HOST,
+    path: path,
+    port: parseInt(process.env.DEEJAY_PORT),
+    method: request.method,
+    headers: request.headers });
+
+  deejay_request.on('response', function(deejay_response) {
+    var status_code = deejay_response.statusCode
+    var recordable = (!deejay_response.headers['content-encoding'])
+    var response_body = ""
+    deejay_response.on('data', function(chunk) {
+      response.write(chunk, 'binary');
+      if (recordable) { response_body += chunk.toString('utf8'); }
+    });
+    deejay_response.on('end', function() {
+      response.end();
+      console.log("Successfully proxied " + request.headers['host'] + path);
+    });
+    response.writeHead(deejay_response.statusCode, deejay_response.headers);
+  });
+  
+}
+
 function request_path(request) {
   var parsed_url = url.parse(request.url);
   return parsed_url['search'] ? (parsed_url['pathname'] + parsed_url['search']) : parsed_url['pathname']
@@ -101,5 +142,12 @@ function record_response(type, url, code, body) {
 function serve(response, body) {
   response.writeHead(200, {'Content-Type': 'text/html',
                            'Content-Length': body.length});
+  response.end(body);
+}
+
+function serve_502(response, body) {
+  response.writeHead(502, {'Content-Type': 'text/html',
+                           'Content-Length': body.length,
+                           'Server': 'Samsara/0.0.1 (node.js)'});
   response.end(body);
 }
